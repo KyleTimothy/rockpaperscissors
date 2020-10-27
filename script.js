2af91c5406601e5ae9e8770ce22b5192bc492368
KyleTimothy <15368505+KyleTimothy@users.noreply.github.com>
2020-10-26 20:01:42 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let rock = document.getElementById("rock")
-let scissors = document.getElementById("scissors")
-let paper = document.getElementById("paper")
-
-rock.addEventListener('click', function(){
-    alert("You clicked rock ");
-})
