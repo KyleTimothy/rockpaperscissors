edc822831125f0be6e51e635d51d22bb3b265341
KyleTimothy <15368505+KyleTimothy@users.noreply.github.com>
2020-10-26 20:02:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let rock = document.getElementById("rock")
+let scissors = document.getElementById("scissors")
+let paper = document.getElementById("paper")
+
+rock.addEventListener('click', function(){
+    alert("You clicked rock ");
+})
